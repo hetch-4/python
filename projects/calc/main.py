26942f8f78540bde5b31d03630454ef563c8f326
hetch-4 <humphreydola@gmail.com>
2025-01-27 23:57:20 +0300

    Calculator

---
+++ projects/calc/main.py
@@ -0,0 +1,34 @@
+import math
+def add(a,b):
+    print( a +b)
+
+def mult(a,b):
+    print( a * b)
+
+def divide(a,b):
+    print(a/b)
+
+def subtract(a,b):
+    print( a - b)
+
+operation =["+","-","*","/"]
+
+a =int(input("enter first no"))
+b = int(input("enter the second no"))
+
+print(operation)
+x = input("Choose your operation")
+print(a,x,b,'=')
+
+    if x =="+":
+        add(a,b)
+    elif x =="-":
+        subtract(a,b)
+    elif x =="/":
+        divide(a,b)
+    elif x == "*":
+        mult(a,b)
+    else :
+        print(error)
+
+print("^-^")    
