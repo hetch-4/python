73f0e6f3196681ba85445ecc783642579e0cdb83
hetch-4 <humphreydola@gmail.com>
2025-01-30 10:07:21 +0300

    initial commit

--- projects/arithmtic_formater/main.py
+++ projects/arithmtic_formater/main.py
@@ -1,44 +1,47 @@
-def arranger(problems, show_answer = False):
+def arithmetic_arranger(problems, show_answers=False):
     if len(problems) > 5:
-        return "Error: Too many problems"
-    fline = []
-    sline = []
+        return "Error: Too many problems."
+
+    first_line = []
+    second_line = []
     dashes = []
     results = []
 
     for problem in problems:
         parts = problem.split()
-        if parts[1] not in ('+', ''):
-            return 'Error: Operator muct be "+" or "-".'
-        
+        if parts[1] not in ('+', '-'):
+            return "Error: Operator must be '+' or '-'."
+
         if not parts[0].isdigit() or not parts[2].isdigit():
-            return "Error: Number must only contain digits"
-        
+            return "Error: Numbers must only contain digits."
+
         if len(parts[0]) > 4 or len(parts[2]) > 4:
-            return "Errror: Numbers cannot be more than four digits."
-        
+            return "Error: Numbers cannot be more than four digits."
+
         num1 = int(parts[0])
         num2 = int(parts[2])
         width = max(len(parts[0]), len(parts[2])) + 2
 
-        fline.append(parts[0].rjust(width))
-        sline.append(parts[1] + parts[2].rjust(width - 1))
+        first_line.append(parts[0].rjust(width))
+        second_line.append(parts[1] + parts[2].rjust(width - 1))
         dashes.append('-' * width)
-
-        if show_answer:
-            result = str(num1 + num2 if parts[1] == '+' else num1 - num2)        
+        
+        if show_answers:
+            result = str(num1 + num2 if parts[1] == '+' else num1 - num2)
             results.append(result.rjust(width))
 
-        arranged_problems ={
-            "  ".join(fline) + '\n' +
-            "  ".join(sline) + '\n' +
-            "  ".join(dashes)
-            }
-        
-        if show_answer:
-            arranged_problems += '\n' + ''.join(results)
+    arranged_problems = (
+        "    ".join(first_line) + "\n" +
+        "    ".join(second_line) + "\n" +
+        "    ".join(dashes)
+    )
 
-        return arranged_problems
+    if show_answers:
+        arranged_problems += "\n" + "    ".join(results)
 
-prob = ['32 + 698', '3301 - 2', '45 + 43', '123 + 49']
-print(arranger(prob))
+    return arranged_problems
+
+
+# Example usage
+problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
+print(arithmetic_arranger(problems, True))
